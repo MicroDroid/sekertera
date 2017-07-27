1c6ffb5718b45a34eac7f4965d1d770355c0be7f
Yousef Sultan <yousef.su.2000@gmail.com>
2017-07-27 21:53:43 +0000

    Add 'pronounce' plugin

---
+++ plugins/pronounce.js
@@ -0,0 +1,29 @@
+var googleTTS = require('google-tts-api');
+
+module.exports = {
+    description: "Echo the pronunciation of a word",
+    
+    handle: message => {
+        const sentence = message.content.split(' ')[1];
+        
+        if (!sentence) {
+            message.channel.send('Add a sentence to pronounce is required');
+            return;
+        }
+            
+        googleTTS(sentence, 'en', 1)
+            .then(url => {
+				if (message.member.voiceChannel)
+					message.member.voiceChannel.join()
+						.then(connection => {
+							const dispatcher = connection.playArbitraryInput(url);
+							dispatcher.on('end', () => {
+								connection.disconnect();
+							});
+						});
+				else message.channel.send(url);
+            }).catch(e => {
+                message.channel.send('An error occurred!');
+            });
+    }
+}
