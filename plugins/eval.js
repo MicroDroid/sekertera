b5d80ccf10ff79732b0ef7b4b9e10e9924997aef
Yousef Sultan <yousef.su.2000@gmail.com>
2017-07-27 21:57:02 +0000

    Add an 'eval' plugin

---
+++ plugins/eval.js
@@ -0,0 +1,22 @@
+const safeEval = require('safe-eval');
+
+module.exports = {
+	adminOnly: true,
+	description: 'Evaluate stuff',
+
+	handle: (message, datastore, bot) => {
+        const expression = message.content.split(' ')[1];
+		var result;
+
+		try {
+			result = safeEval(expression, {
+				env: process.env,
+				bot, datastore
+			});
+		} catch (e) {
+			result = e;
+		}
+
+		message.channel.send('```\n' + result + '\n```');
+	}
+}
